65deb95c910a0aef8ce9d118c0959df38796e3bf
Kannan-Asokan <142194993+Kannan-Asokan@users.noreply.github.com>
2023-08-14 09:23:59 +0530

    Add files via upload

---
+++ Google_clone _project/program_files/main.js
@@ -0,0 +1,33 @@
+function mygmail() {
+    open('https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&emr=1&ltmpl=default&ltmplcache=2&osid=1&passive=true&rm=false&scc=1&service=mail&ss=1&ifkv=AeDOFXiUhc-fNaZRa_zYVY3WXFdBsplZWmk_srTNPGjRcmScNiOONNbmZpNaJdOPDOTeswA-JtUBdA&flowName=GlifWebSignIn&flowEntry=ServiceLogin','_self');
+}
+function gimages() {
+    open('https://images.google.com/','_self');
+}
+function signin() {
+    open('https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fimages.google.com%2F&ec=GAZAAg&hl=en&passive=true&ifkv=AeDOFXhY1vdsAtMmQt5cs3c3g1M1MsRw0Owf5igxQuJVykXKZX-GOo98Qss1RDBzLXnU5oQ-NDK_SQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin','_self');
+}
+function gapps() {
+    alert("This is a sample google clone project.");
+}
+function mydrive() {
+    open('https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fdrive.google.com%2Fdrive%2Fmy-drive&followup=https%3A%2F%2Fdrive.google.com%2Fdrive%2Fmy-drive&osid=1&passive=1209600&service=wise&ifkv=AeDOFXjiqoNwOXgq0cHR9Lq21VJXik8vlyKtErssuNe12-Wj7tnejNJFBnXQNe59AUBC8iqa7dMGwQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin','_self');
+}
+function myyoutube() {
+    open('https://www.youtube.com/','_self');
+}
+function mywhatsapp() {
+    open('https://web.whatsapp.com/','_self');
+}
+function myinstagram() {
+    open('https://www.instagram.com/','_self');
+}
+function mywikipedia() {
+    open('https://www.wikipedia.org/','_self');
+}
+function mywebstore() {
+    open('https://chrome.google.com/webstore/category/extensions','_self');
+}
+function myamazon() {
+    open('https://www.amazon.in/?tag=msndeskstdin-21&ref=pd_sl_7eia7h1iv9_e&adgrpid=1316117075550739&hvadid=82257581515009&hvnetw=o&hvqmt=e&hvbmt=be&hvdev=c&hvlocint=&hvlocphy=1674&hvtargid=kwd-82258246070633:loc-90&hydadcr=14451_2334183','_self');
+}
